f9b6be9605414c9656af00f9aded23c10bb63846
YTFTF <yunghy1@chungbuk.ac.kr>
2024-04-12 23:21:10 +0900

    week 1 실습 연습

---
+++ 0_1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(){
+    std :: cout << "My name is Park" << '\n';
+
+    return 0;
+}
---
+++ 0_2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    std :: cout << "소프트웨어 학부" << '\n';
+    std :: cout << "21세" << '\n';
+    std :: cout << "SW개발자" << '\n';
+    
+    return 0;
+}
