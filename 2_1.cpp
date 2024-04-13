2999e12ff6ab53fbda1b86869b3fd0129475954b
YTFTF <yunghy1@chungbuk.ac.kr>
2024-04-13 23:30:44 +0900

    week 3 연습

---
+++ 2_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main (){
+    int M[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    int (*ptr)[3]; int *p; int **pt;
+
+    ptr = M;
+    cout << ptr << M ;
+    cout << ptr+1 << M+1;
+    cout << *(ptr+1) << ptr[1] << *(M+1) << M[1] ;
+    cout << **(ptr+1) << **(M+1) << *M[1] << M[1][0];
+
+    p = M[0];
+    cout << p << M[0] << *M;
+    cout << p+1 << M[0]+1 << *M+1;
+    cout << *(p+1) << *(M[0]+1) << *(*M+1);
+    
+    pt = &p; // pt = M; (X)
+    cout << *pt << p ;
+    cout << **pt << *p;
+}
