4310b0da1f544f7371f229799ec9428e11a354c8
YTFTF <yunghy1@chungbuk.ac.kr>
2024-04-13 22:32:50 +0900

    week2 실습 연습

--- 1_4.cpp
+++ 1_4.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main() {
     char c[100]; // 한 라인에 100개의 문자면 충분하다.
     int count = 0;
-    cin.getline(c);
-    
-
-    
     cout << "문자들을 입력하라(100개 미만)." << endl;
+    cin.getline(c,100);
+    for(int i=0;i<strlen(c);i++){
+        if(c[i]=='x'){
+            count += 1;
+        }
+    }
     cout << 'x' << "의 개수는 " << count << endl;
 }
